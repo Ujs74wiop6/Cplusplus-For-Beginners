8ab4e95f023768a2e94d1a6c7ffd51c54e4876e0
Fabricio Tiago Arantes <fabricioujs74@gmail.com>
2022-11-21 03:45:37 -0300

    First Commit

--- 04_Namespaces/main.cpp
+++ 04_Namespaces/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+namespace first {
+    int x = 1;
+}
+namespace second {
+    int x = 2;
+}
+
+// Namespace = Provides a solution for preventing name conflicts in large project.
+
+using namespace std;
+
+/* using namespace ... first or second*/
+
 int main() {
-    
     int x = 0;
+
+    string name = "Fabricio";
+
+    cout << name << endl;
+    cout << x << endl;
 
     return 0;
 }
