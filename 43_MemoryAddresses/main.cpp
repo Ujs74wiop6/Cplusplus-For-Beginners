d64c8a547d55ea42ab3713751433954ba4f1e451
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2023-01-05 23:57:03 -0300

    Update 05/01/2023

---
+++ 43_MemoryAddresses/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+//Memory Addresses
+
+string name = "Fabricio";
+int age = 20;
+bool student = true;
+
+cout << &name << endl;
+cout << &age << endl;
+cout << &student << endl;
+
+return 0;
+}
